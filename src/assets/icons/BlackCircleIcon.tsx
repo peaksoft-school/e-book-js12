70e41ce73b446f819e89da80ea4d8cdfd8a2ba56
XaiaAibek <aibekxairullauulu@gmail.com>
2024-04-04 21:27:35 +0600

    new icon

---
+++ src/assets/icons/BlackCircleIcon.tsx
@@ -0,0 +1,18 @@
+const BlackCircleIcon = () => {
+	return (
+		<>
+			<svg
+				width="26"
+				height="26"
+				viewBox="0 0 26 26"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<circle cx="13" cy="13" r="12.5" stroke="#F34901" />
+				<circle cx="13" cy="13" r="8.5" fill="#F34901" stroke="#F34901" />
+			</svg>
+		</>
+	);
+};
+
+export default BlackCircleIcon;
