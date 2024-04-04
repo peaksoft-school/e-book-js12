cc9720cd87b0523ba59322564eacadf0f6a65722
MahmudovIbragim <ibrahimorunbaev59@gmail.com>
2024-04-04 14:22:28 +0600

    added file icnos

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,9 @@
+export { default as IconOrder } from './order';
+export { default as IconLogin } from './login';
+export { default as IconHeart } from './heart';
+export { default as IconHeartActive } from './heart_active';
+export { default as IconFilter } from './filter';
+export { default as IconLogout } from './logout';
+export { default as IconUser } from './user';
+export { default as IconC } from './cLogo';
+export { default as IconLogo } from './logo';
