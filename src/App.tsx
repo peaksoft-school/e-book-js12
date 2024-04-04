3fcf57804962537798506b8c363565ee0b1b793f
MahmudovIbragim <ibrahimorunbaev59@gmail.com>
2024-04-04 14:53:50 +0600

    update start-project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Route, Routes } from 'react-router';
-import LayoutUser from './pagesUser/components/layout/LayoutUser';
-import LayoutAdmin from './pagesAdmin/components/layout/LayoutAdmin';
-import LayoutAuth from './pagesAuth/components/layout/LayoutAuth';
+import LayoutVender from './venderRole/components/layout/LayoutVender';
+import LayoutUser from './userRole/components/layout/LayoutUser';
+import LayoutAdmin from './adminRole/components/layout/LayoutAdmin';
+import LayoutAuth from './authRole/components/layout/LayoutAuth';
 
 const App = () => {
 	return (
@@ -10,6 +11,7 @@ const App = () => {
 				<Route path="/*" element={<LayoutUser />} />
 				<Route path="/admin/*" element={<LayoutAdmin />} />
 				<Route path="/auth/*" element={<LayoutAuth />} />
+				<Route path="/vender/*" element={<LayoutVender />} />
 			</Routes>
 		</>
 	);
