5056fa7dbeaf1cc7cdce80029ada5d199ebeabf5
Khaliza <nasredinovahaliza@gmail.com>
2024-04-05 16:01:21 +0600

    updated custom-button

--- src/ui/customButton/CustomAddBook.tsx
+++ src/ui/customButton/CustomAddBook.tsx
@@ -8,10 +8,7 @@ interface CustomAddBookProps {
 
 const CustomAddBookButton: FC<CustomAddBookProps> = ({ children, onClick }) => {
 	return (
-		<button
-			className={`${scss.customButtonAddBook}  ${children}`}
-			onClick={onClick}
-		>
+		<button className={`${scss.customButtonAddBook}`} onClick={onClick}>
 			{children}
 		</button>
 	);
