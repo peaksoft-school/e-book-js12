9e92127939c2510e26c7d6cf67a37a85c3a62381
unknown <almazovich00108@gmail.com>
2024-04-07 14:39:42 +0600

    bugfix inputs

--- src/pagesUser/components/pages/HomePage.tsx
+++ src/pagesUser/components/pages/HomePage.tsx
@@ -4,6 +4,7 @@ const HomePage = () => {
 	return (
 		<>
 			<Welcome />
+			
 		</>
 	);
 };
--- src/ui/customInpute/CustomPasswordInput.tsx
+++ src/ui/customInpute/CustomPasswordInput.tsx
@@ -1,6 +1,7 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import scss from './Style.module.scss';
-import EyeSeeIcon from '@/src/assets/icons/EyeSeeIcon';
+import EyeSeeIcon from '@/src/assets/icons/icon-eyeSee';
+import EyeClose from '@/src/assets/icons/icon-eyeClose';
 
 interface TypeProps {
 	placeholder: string;
@@ -8,14 +9,38 @@ interface TypeProps {
 }
 
 const CustomPasswordInput: FC<TypeProps> = ({ placeholder, type }) => {
+	const [showPassword, setShowPassword] = useState(true);
+	const [icon, setIcon] = useState(<EyeSeeIcon />);
+	const [isFocused, setIsFocused] = useState(false);
+
+	const togglePasswordVisibility = () => {
+		setShowPassword(!showPassword);
+		setIcon(showPassword ? <EyeClose /> : <EyeSeeIcon />);
+	};
+
+	const handleFocus = () => {
+		setIsFocused(true);
+	};
+
+	const handleBlur = () => {
+		setIsFocused(false);
+	};
+
 	return (
 		<div className={scss.passwordContainer}>
 			<input
 				placeholder={placeholder}
-				type={type}
-				className={scss.passwordInput}
+				type={showPassword ? 'text' : type}
+				className={`${scss.passwordInput} ${isFocused ? scss.FocusedIcon : ''}`}
+				onFocus={handleFocus}
+				onBlur={handleBlur}
 			/>
-			<EyeSeeIcon />
+			<div
+				onClick={togglePasswordVisibility}
+				className={`${scss.EyeSeeIcon} ${isFocused ? scss.FocusedIcon : ''}`}
+			>
+				{icon}
+			</div>
 		</div>
 	);
 };
