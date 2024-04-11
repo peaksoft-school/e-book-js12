42f8f024b7004ce0672f11e8dc8dacef39c6baef
Khaliza <nasredinovahaliza@gmail.com>
2024-04-11 20:15:45 +0600

    bugfix welcome

--- src/pagesUser/components/pages/homeSections/Welcome.tsx
+++ src/pagesUser/components/pages/homeSections/Welcome.tsx
@@ -1,7 +1,5 @@
 import { FC } from 'react';
 import scss from './Welcome.module.scss';
-import AudioBooks from '../audioBooks/AudioBooks';
-import SubscribePage from '../subscribePage/SubscribePage';
 
 const Welcome: FC = () => {
 	return (
@@ -10,8 +8,6 @@ const Welcome: FC = () => {
 				<div className="container">
 					<div className={scss.content}>
 						<h1>Welcome, User!</h1>
-						<AudioBooks />
-						<SubscribePage />
 					</div>
 				</div>
 			</section>
