be452aaf0102466b5eb6aaa17072fbca3cd5b7a5
unknown <almazovich00108@gmail.com>
2024-04-12 21:09:43 +0600

    created-third-section

---
+++ src/assets/icons/icon-short-line.tsx
@@ -0,0 +1,15 @@
+const shortLine = () => {
+	return (
+		<svg
+			width="396"
+			height="1"
+			viewBox="0 0 396 1"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<line x1="0.505859" y1="0.5" x2="395.506" y2="0.5" stroke="#E24604" />
+		</svg>
+	);
+};
+
+export default shortLine;
