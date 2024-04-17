26f8746cc4839d72bbbf94ceef4c165c8c724364
Khaliza <nasredinovahaliza@gmail.com>
2024-04-17 19:01:55 +0600

    update

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -44,8 +44,6 @@ export { default as IconWhiteUsersIcon } from './icon-whiteUsersIcon';
 export { default as IconDownloaded } from './icon-downloaded';
 export { default as IconDownload } from './icon-download';
 export { default as IconDownloading } from './icon-downloading';
-export {default as IconAddPhoto} from './add-photo-icon';
+export { default as IconAddPhoto } from './add-photo-icon';
 export { default as IconRedDot } from './icon-redDot';
-export {default as IconBurgerMenu} from './icon-burger-menu';
-
-
+export { default as IconBurgerMenu } from './icon-burger-menu';
