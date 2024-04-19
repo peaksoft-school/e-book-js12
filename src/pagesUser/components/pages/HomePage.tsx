9355a96d12c25f5b635956a61f2101befdfe06c9
MahmudovIbragim <ibrahimorunbaev59@gmail.com>
2024-04-20 00:36:03 +0600

    merge dev

--- src/pagesUser/components/pages/HomePage.tsx
+++ src/pagesUser/components/pages/HomePage.tsx
@@ -1,9 +1,13 @@
+import AudioBooks from './audioBooks/AudioBooks';
+import Ebooks from './eBooks/Ebooks';
 import Welcome from './homeSections/Welcome';
 
 const HomePage = () => {
 	return (
 		<>
 			<Welcome />
+			<AudioBooks />
+			<Ebooks />
 		</>
 	);
 };
