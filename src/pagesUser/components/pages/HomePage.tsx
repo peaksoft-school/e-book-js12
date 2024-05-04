0ad54df20ada2e2230e0f1059253708b4f93065d
Ars1anny~ <moldosanovaaida@gmail.com>
2024-05-04 13:10:10 +0600

    added user-page

--- src/pagesUser/components/pages/HomePage.tsx
+++ src/pagesUser/components/pages/HomePage.tsx
@@ -1,19 +1,21 @@
-import LatestBookSection from './homeSections/LatesBookSection';
-import BestsellerSection from './homeSections/BestsellerSection';
-import LastPublicationSection from './homeSections/LastPublicationSection';
-import AudioBookSection from './homeSections/AudioBookSection';
-import EbookSection from './homeSections/EbookSection';
-import SubscribeSection from './homeSections/SubscribeSection';
+// import LatestBookSection from './homeSections/LatesBookSection';
+// import BestsellerSection from './homeSections/BestsellerSection';
+// import LastPublicationSection from './homeSections/LastPublicationSection';
+// import AudioBookSection from './homeSections/AudioBookSection';
+// import EbookSection from './homeSections/EbookSection';
+// import SubscribeSection from './homeSections/SubscribeSection';
+import LayoutAdmin from '@/src/pagesAdmin/components/layout/LayoutAdmin';
 
 const HomePage = () => {
 	return (
 		<>
-			<LatestBookSection />
+			<LayoutAdmin />
+			{/* <LatestBookSection />
 			<BestsellerSection />
 			<LastPublicationSection />
 			<AudioBookSection />
 			<EbookSection />
-			<SubscribeSection />
+			<SubscribeSection /> */}
 		</>
 	);
 };
