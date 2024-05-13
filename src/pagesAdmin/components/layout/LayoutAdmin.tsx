a14951f131ba6858175d15349ad38dbe9107e191
XaiaAibek <aibekxairullauulu@gmail.com>
2024-05-13 15:03:41 +0600

    bagfix vendor-book

--- src/pagesAdmin/components/layout/LayoutAdmin.tsx
+++ src/pagesAdmin/components/layout/LayoutAdmin.tsx
@@ -22,9 +22,9 @@ const LayoutAdmin = () => {
 						<Route path="/inner/:bookInfo" element={<BookInfo />} />
 						<Route path="/vendors" element={<VendorsPage />} />
 						<Route path="/users" element={<UserPage />} />
+						
 						<Route path="/books" element={<BooksPage />} />
-						<Route path="/books/book_adding" element={<AddBookPage />} />
-
+						<Route path="/books/book_adding" element={<AddBookPage />} />						
 						<Route path="/vendors/:name" element={<AboutVendor />} />
 					</Routes>
 				</main>
