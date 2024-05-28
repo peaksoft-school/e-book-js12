8d7313308675edc9b918708451715ed0249dcd69
XaiaAibek <aibekxairullauulu@gmail.com>
2024-05-28 17:15:34 +0600

    merge

--- src/ui/customInpute/CustomUserNameInput.tsx
+++ src/ui/customInpute/CustomUserNameInput.tsx
@@ -3,24 +3,24 @@ import { FC } from 'react';
 import scss from './Style.module.scss';
 
 interface TypeProps {
-  placeholder: string;
-  registerName: string;
-  register: any;
+	placeholder: string;
+	registerName: string;
+	register: any;
 }
 
 const CustomUserNameInput: FC<TypeProps> = ({
-  placeholder,
-  registerName,
-  register
+	placeholder,
+	registerName,
+	register
 }) => {
-  return (
-    <input
-      type="text"
-      placeholder={placeholder}
-      className={scss.user_name_input}
-      {...register(registerName)}
-    />
-  );
+	return (
+		<input
+			type="text"
+			placeholder={placeholder}
+			className={scss.user_name_input}
+			{...register(registerName)}
+		/>
+	);
 };
 
 export default CustomUserNameInput;
