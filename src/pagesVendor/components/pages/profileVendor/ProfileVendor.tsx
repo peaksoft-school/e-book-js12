c9c6fe63d04a2cf181b66c6dea0da2913d903965
XaiaAibek <aibekxairullauulu@gmail.com>
2024-05-28 17:11:33 +0600

    profile

--- src/pagesVendor/components/pages/profileVendor/ProfileVendor.tsx
+++ src/pagesVendor/components/pages/profileVendor/ProfileVendor.tsx
@@ -1,83 +1,160 @@
+import React, { useState } from 'react';
 import CustomUserNameInput from '@/src/ui/customInpute/CustomUserNameInput';
-import scss from './ProfileVendor.module.scss';
 import CustomPasswordInput from '@/src/ui/customInpute/CustomPasswordInput';
-import { useForm } from 'react-hook-form';
+import scss from './ProfileVendor.module.scss';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-const ProfileVendor = () => {
-	const { register } = useForm();
-	return (
-		<div className="container">
-			<div className={scss.section_profile}>
-				<div className={scss.profile_info_name}>
-					<div className={scss.profile_box}>
-						<div className={scss.profile_name}>
-							<h4 className={scss.text_tree}>Личная информация</h4>
-							<div className={scss.input_name_info}>
-								<p className={scss.name_info_text}>Мое имя</p>
-								<CustomUserNameInput placeholder="Напишите ваше имя" />
-							</div>
-							<div className={scss.input_sor_name_info}>
-								<p className={scss.sor_name_info_text}>Ваша фамилие</p>
-								<CustomUserNameInput placeholder="Введите вашу фамилию" />
-							</div>
-							<div className={scss.input_phone_info}>
-								<p className={scss.phone_info_text}>Номер телефона</p>
-								<CustomUserNameInput placeholder="+996 (___) __ __ __" />
-							</div>
+interface MyForm {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  email: string;
+  currentPassword: string;
+  newPassword: string;
+  confirmPassword: string;
+}
 
-							<div>
-								<p className={scss.email_text}>Email</p>
-								<CustomUserNameInput placeholder="Напишите ваш Email" />
-							</div>
-							<p className={scss.tex}>Удалить профиль?</p>
-						</div>
-						<div className={scss.section_new_password}>
-							<div>
-								<h4 className={scss.change_password}>Изменить пароль</h4>
-							</div>
-							<div className={scss.new_password}>
-								<div className={scss.input_new_password}>
-									<p className={scss.current_password}>Текущий пароль</p>
-									<CustomPasswordInput
-										type="password"
-										placeholder="Напишите текущий пароль"
-										register={register}
-										registerName={'ff'}
-									/>
-								</div>
-								<div className={scss.input_new_password}>
-									<p className={scss.dowland_new_password}>Новый пароль</p>
-									<CustomPasswordInput
-										type="password"
-										placeholder="Напишите новый пароль"
-										register={register}
-										registerName={'ff'}
-									/>
-								</div>
-								<div className={scss.input_new_password}>
-									<p className={scss.confirm_password}>Подтвердите пароль</p>
-									<CustomPasswordInput
-										type="password"
-										placeholder="Подтвердите пароль"
-										register={register}
-										registerName={'ff'}
-									/>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className={scss.button_section}>
-						<div className={scss.button_note}>
-							<button className={scss.custom_white_button}>Отменить</button>
-						</div>
-						<div>
-							<button className={scss.custom_black_button}>Сохранить</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+const ProfileVendor: React.FC = () => {
+  const { register, handleSubmit } = useForm<MyForm>({
+    defaultValues: {
+      firstName: 'Айбек',
+      lastName: 'Хайрулла уулу',
+      phoneNumber: '500815251',
+      email: 'aibekxairullauulu@gmail.com',
+      currentPassword: '',
+      newPassword: '',
+      confirmPassword: '',
+    },
+  });
+
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const toggleEditMode = () => {
+    setIsEditMode(!isEditMode);
+  };
+
+  const onSubmit: SubmitHandler<MyForm> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={scss.section_profile}>
+          <div className={scss.profile_info_name}>
+            <div className={scss.profile_box}>
+              <div className={scss.profile_name}>
+                <h4 className={scss.text_tree}>Личная информация</h4>
+                <div className={scss.input_name_info}>
+                  <p className={scss.name_info_text}>Мое имя</p>
+                  {isEditMode ? (
+                    <CustomUserNameInput
+                      placeholder="Напишите ваше имя"
+                      register={register}
+                      registerName="firstName"
+                    />
+                  ) : (
+                    <p>Айбек</p>
+                  )}
+                </div>
+                <div className={scss.input_sor_name_info}>
+                  <p className={scss.sor_name_info_text}>Ваша фамилия</p>
+                  {isEditMode ? (
+                    <CustomUserNameInput
+                      placeholder="Введите вашу фамилию"
+                      register={register}
+                      registerName="lastName"
+                    />
+                  ) : (
+                    <p>Хайрулла уулу</p>
+                  )}
+                </div>
+                <div className={scss.input_phone_info}>
+                  <p className={scss.phone_info_text}>Номер телефона</p>
+                  {isEditMode ? (
+                    <CustomUserNameInput
+                      placeholder="+996 (___) __ __ __"
+                      register={register}
+                      registerName="phoneNumber"
+                    />
+                  ) : (
+                    <p>500815251</p>
+                  )}
+                </div>
+                <div>
+                  <p className={scss.email_text}>Email</p>
+                  {isEditMode ? (
+                    <CustomUserNameInput
+                      placeholder="Напишите ваш Email"
+                      register={register}
+                      registerName="email"
+                    />
+                  ) : (
+                    <p>aibekxairullauulu@gmail.com</p>
+                  )}
+                </div>
+                <p className={scss.tex}>Удалить профиль?</p>
+              </div>
+              {isEditMode && (
+                <div className={scss.section_new_password}>
+                  <div>
+                    <h4 className={scss.change_password}>Изменить пароль</h4>
+                  </div>
+                  <div className={scss.new_password}>
+                    <div className={scss.input_new_password}>
+                      <p className={scss.current_password}>Текущий пароль</p>
+                      <CustomPasswordInput
+                        type="password"
+                        placeholder="Напишите текущий пароль"
+                        register={register}
+                        registerName="currentPassword"
+                      />
+                    </div>
+                    <div className={scss.input_new_password}>
+                      <p className={scss.dowland_new_password}>Новый пароль</p>
+                      <CustomPasswordInput
+                        type="password"
+                        placeholder="Напишите новый пароль"
+                        register={register}
+                        registerName="newPassword"
+                      />
+                    </div>
+                    <div className={scss.input_new_password}>
+                      <p className={scss.confirm_password}>Подтвердите пароль</p>
+                      <CustomPasswordInput
+                        type="password"
+                        placeholder="Подтвердите пароль"
+                        register={register}
+                        registerName="confirmPassword"
+                      />
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className={scss.button_section}>
+              <div className={scss.button_note}>
+                <button
+                  type="button"
+                  className={scss.custom_white_button}
+                  onClick={toggleEditMode}
+                >
+                  {isEditMode ? 'Отменить' : 'Изменить'}
+                </button>
+              </div>
+              {isEditMode && (
+                <div>
+                  <button type="submit" className={scss.custom_black_button}>
+                    Сохранить
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ProfileVendor;
